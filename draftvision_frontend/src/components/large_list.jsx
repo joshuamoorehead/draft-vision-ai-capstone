d29f583c7a78fe9c7193c27aa3c4307a3bd52149
ryankarbow <karbownr@email.sc.edu>
2024-11-13 13:00:43 -0500

    Frontend Prototyping

---
+++ draftvision_frontend/src/components/large_list.jsx
@@ -0,0 +1,128 @@
+<div style={{width: '100%', height: '100%', position: 'relative', background: '#5A6BB0'}}>
+    <div style={{width: 625, height: 77, left: 408, top: 231, position: 'absolute'}}>
+        <div style={{width: 507, height: 77, left: 57, top: 0, position: 'absolute', background: '#F5F5F5', borderRadius: 13, border: '3px black solid'}} />
+        <div style={{width: 625, height: 47, left: 0, top: 10, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 48, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>2025 Draft Large List</div>
+    </div>
+    <div style={{width: 856, height: 127, left: 295, top: 408, position: 'absolute'}}>
+        <div style={{width: 856, height: 127, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', border: '3px black solid'}}></div>
+        <img style={{width: 98, height: 88, left: 79, top: 20, position: 'absolute', borderRadius: 9999}} src="https://via.placeholder.com/98x88" />
+        <div style={{width: 76, height: 74, left: 3, top: 26, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '600', lineHeight: 28.80, wordWrap: 'break-word'}}>1</div>
+        <div style={{width: 168, left: 187, top: 12, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Travis Hunter</div>
+        <div style={{width: 168, left: 604, top: 12, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Player Rating:</div>
+        <div style={{width: 168, left: 604, top: 50, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>99.8</div>
+        <div style={{width: 168, left: 187, top: 49, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Colorado</div>
+        <div style={{width: 168, left: 187, top: 84, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>WR/CB</div>
+        <div style={{width: 48, height: 48, left: 786, top: 48, position: 'absolute'}}>
+            <div style={{width: 28, height: 28, left: 10, top: 10, position: 'absolute', border: '4px #1E1E1E solid'}}></div>
+        </div>
+    </div>
+    <div style={{width: 856, height: 127, left: 295, top: 952, position: 'absolute'}}>
+        <div style={{width: 856, height: 127, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', border: '3px black solid'}}></div>
+        <img style={{width: 98, height: 88, left: 79, top: 20, position: 'absolute', borderRadius: 9999}} src="https://via.placeholder.com/98x88" />
+        <div style={{width: 76, height: 74, left: 3, top: 26, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '600', lineHeight: 28.80, wordWrap: 'break-word'}}>5</div>
+        <div style={{width: 196, left: 173, top: 11, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Shedeur Sanders</div>
+        <div style={{width: 168, left: 187, top: 49, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Colorado</div>
+        <div style={{width: 168, left: 187, top: 84, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>WR/CB</div>
+        <div style={{width: 168, left: 594, top: 11, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Player Rating:</div>
+        <div style={{width: 168, left: 594, top: 49, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>93.7</div>
+        <div style={{width: 48, height: 48, left: 776, top: 47, position: 'absolute'}}>
+            <div style={{width: 28, height: 28, left: 10, top: 10, position: 'absolute', border: '4px #1E1E1E solid'}}></div>
+        </div>
+    </div>
+    <div style={{width: 856, height: 127, left: 295, top: 816, position: 'absolute'}}>
+        <div style={{width: 856, height: 127, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', border: '3px black solid'}}></div>
+        <img style={{width: 98, height: 88, left: 79, top: 20, position: 'absolute', borderRadius: 9999}} src="https://via.placeholder.com/98x88" />
+        <div style={{width: 76, height: 74, left: 3, top: 26, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '600', lineHeight: 28.80, wordWrap: 'break-word'}}>4</div>
+        <div style={{width: 168, left: 187, top: 13, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Seven McGee</div>
+        <div style={{width: 168, left: 187, top: 50, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Albany</div>
+        <div style={{width: 168, left: 187, top: 85, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>WR</div>
+        <div style={{width: 168, left: 594, top: 13, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Player Rating:</div>
+        <div style={{width: 168, left: 594, top: 51, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>95.4</div>
+        <div style={{width: 48, height: 48, left: 776, top: 49, position: 'absolute'}}>
+            <div style={{width: 28, height: 28, left: 10, top: 10, position: 'absolute', border: '4px #1E1E1E solid'}}></div>
+        </div>
+    </div>
+    <div style={{width: 856, height: 127, left: 295, top: 680, position: 'absolute'}}>
+        <div style={{width: 856, height: 127, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', border: '3px black solid'}}></div>
+        <img style={{width: 98, height: 88, left: 79, top: 20, position: 'absolute', borderRadius: 9999}} src="https://via.placeholder.com/98x88" />
+        <div style={{width: 76, height: 74, left: 3, top: 26, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '600', lineHeight: 28.80, wordWrap: 'break-word'}}>3</div>
+        <div style={{width: 168, left: 187, top: 16, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Kyle Mccord</div>
+        <div style={{width: 168, left: 187, top: 53, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Syracuse</div>
+        <div style={{width: 168, left: 187, top: 88, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>QB</div>
+        <div style={{width: 168, left: 594, top: 11, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Player Rating:</div>
+        <div style={{width: 168, left: 594, top: 49, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>95.9</div>
+        <div style={{width: 48, height: 48, left: 776, top: 47, position: 'absolute'}}>
+            <div style={{width: 28, height: 28, left: 10, top: 10, position: 'absolute', border: '4px #1E1E1E solid'}}></div>
+        </div>
+    </div>
+    <div style={{width: 856, height: 127, left: 295, top: 544, position: 'absolute'}}>
+        <div style={{width: 856, height: 127, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', border: '3px black solid'}}></div>
+        <img style={{width: 98, height: 88, left: 79, top: 20, position: 'absolute', borderRadius: 9999}} src="https://via.placeholder.com/98x88" />
+        <div style={{width: 76, height: 74, left: 3, top: 26, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '600', lineHeight: 28.80, wordWrap: 'break-word'}}>2</div>
+        <div style={{width: 196, left: 173, top: 12, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Raheim Sanders</div>
+        <div style={{width: 168, left: 187, top: 50, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>South Carolina</div>
+        <div style={{width: 168, left: 187, top: 85, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>RB</div>
+        <div style={{width: 168, left: 604, top: 8, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Player Rating:</div>
+        <div style={{width: 168, left: 604, top: 46, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 36, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>97.6</div>
+        <div style={{width: 48, height: 48, left: 786, top: 44, position: 'absolute'}}>
+            <div style={{width: 28, height: 28, left: 10, top: 10, position: 'absolute', border: '4px #1E1E1E solid'}}></div>
+        </div>
+    </div>
+    <div style={{width: 240, height: 624, left: 0, top: 408, position: 'absolute'}}>
+        <div style={{width: 240, height: 624, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', border: '3px black solid'}} />
+        <div style={{width: 168, left: 36, top: 18, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Filters</div>
+        <div style={{width: 141, height: 24, left: 49, top: 80, position: 'absolute', background: 'white', borderRadius: 5, border: '2px black solid'}} />
+        <div style={{width: 16, height: 14, left: 170, top: 84, position: 'absolute'}}>
+            <div style={{width: 9.33, height: 8.17, left: 3.33, top: 2.92, position: 'absolute', border: '2px #1E1E1E solid'}}></div>
+        </div>
+        <div style={{width: 50, height: 8, left: 53, top: 84, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 13, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Select</div>
+        <div style={{width: 70, height: 18, left: 30, top: 60, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Year</div>
+        <div style={{width: 141, height: 24, left: 49, top: 300, position: 'absolute', background: 'white', borderRadius: 5, border: '2px black solid'}} />
+        <div style={{width: 16, height: 14, left: 170, top: 304, position: 'absolute'}}>
+            <div style={{width: 9.33, height: 8.17, left: 3.33, top: 2.92, position: 'absolute', border: '2px #1E1E1E solid'}}></div>
+        </div>
+        <div style={{width: 46, height: 8, left: 55, top: 304, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 13, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Select</div>
+        <div style={{width: 70, height: 14, left: 35, top: 281, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Team</div>
+        <div style={{width: 141, height: 24, left: 49, top: 224, position: 'absolute', background: 'white', borderRadius: 5, border: '2px black solid'}} />
+        <div style={{width: 16, height: 14, left: 170, top: 230, position: 'absolute'}}>
+            <div style={{width: 9.33, height: 8.17, left: 3.33, top: 2.92, position: 'absolute', border: '2px #1E1E1E solid'}}></div>
+        </div>
+        <div style={{width: 46, height: 8, left: 55, top: 227, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 13, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Select</div>
+        <div style={{width: 95, height: 14, left: 45, top: 204, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Conference</div>
+        <div style={{width: 141, height: 24, left: 49, top: 153, position: 'absolute', background: 'white', borderRadius: 5, border: '2px black solid'}} />
+        <div style={{width: 16, height: 14, left: 170, top: 157, position: 'absolute'}}>
+            <div style={{width: 9.33, height: 8.17, left: 3.33, top: 2.92, position: 'absolute', border: '2px #1E1E1E solid'}}></div>
+        </div>
+        <div style={{width: 46, height: 8, left: 55, top: 157, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 13, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Select</div>
+        <div style={{width: 70, height: 14, left: 44, top: 134, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Position</div>
+    </div>
+    <div style={{width: 1440, height: 163, left: 0, top: 0, position: 'absolute'}}>
+        <div style={{width: 1440, height: 130, left: 0, top: 0, position: 'absolute', background: 'black'}}>
+            <div style={{width: 669.04, height: 60.85, left: 308, top: 28, position: 'absolute'}}>
+                <div style={{width: 169.21, height: 38, left: 245.24, top: 11, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Roboto Condensed', fontWeight: '400', textDecoration: 'underline', wordWrap: 'break-word'}}>Mock Draft</div>
+                <div style={{width: 143.12, height: 39, left: 0, top: 11, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Roboto Condensed', fontWeight: '400', textDecoration: 'underline', wordWrap: 'break-word'}}>Large List</div>
+                <div style={{width: 173.04, height: 60.85, paddingLeft: 21.77, paddingRight: 21.77, paddingTop: 10.88, paddingBottom: 10.88, left: 496, top: 0, position: 'absolute', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+                    <div style={{justifyContent: 'center', alignItems: 'center', gap: 10.88, display: 'inline-flex'}}>
+                        <div style={{width: 24.49, height: 28.57, position: 'relative'}}>
+                            <div style={{width: 9.50, height: 20.63, left: 2.75, top: 0.30, position: 'absolute', background: 'black'}}></div>
+                        </div>
+                        <div style={{color: '#F6F3F3', fontSize: 30, fontFamily: 'Roboto Condensed', fontWeight: '400', lineHeight: 34.28, textDecoration: 'underline', wordWrap: 'break-word'}}>Players</div>
+                    </div>
+                    <div style={{width: 129.50, height: 4.08, position: 'relative'}}>
+                        <div style={{width: 207.97, height: 4.08, left: 0, top: 0, position: 'absolute', background: 'white'}} />
+                    </div>
+                </div>
+            </div>
+            <img style={{width: 188, height: 188, left: 6, top: -29, position: 'absolute', boxShadow: '0px 0.7520000338554382px 0.7520000338554382px rgba(0, 0, 0, 0.25)', border: '0.19px black solid'}} src="https://via.placeholder.com/188x188" />
+            <div style={{height: 36, left: 1104, top: 45, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex'}}>
+                <div style={{alignSelf: 'stretch', height: 36, padding: 8, background: 'rgba(250, 250, 250, 0.93)', borderRadius: 10, justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
+                    <div style={{width: 28, height: 28, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                        <div style={{width: 28, height: 28, textAlign: 'center', color: 'rgba(60, 60, 67, 0.60)', fontSize: 18, fontFamily: 'SF Pro Display', fontWeight: '500', lineHeight: 50, wordWrap: 'break-word'}}>􀊫</div>
+                    </div>
+                    <div style={{flex: '1 1 0', color: 'rgba(60, 60, 67, 0.60)', fontSize: 17, fontFamily: 'SF Pro Text', fontWeight: '400', lineHeight: 22, wordWrap: 'break-word'}}>Search</div>
+                </div>
+            </div>
+        </div>
+        <div style={{width: 130, height: 0, left: 254, top: 130, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', border: '3px white solid'}}></div>
+    </div>
+</div>
