8d973a7e7c2ba0030550842b5112016cb0d6143a
Tyler Super <tsuper@email.sc.edu>
2024-11-17 17:33:35 -0500

    More backend development w/ work in urls.py and views.py

--- draftvision_backend/api/tests.py
+++ draftvision_backend/api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Player
+
 
 # Create your tests here.
+
+class PlayerAPITest(APITestCase):
+    def setUp(self):
+        #Create test player with name position and college filled out, all other parameters default
+        Player.objects.create(name="Joe Football", college="LSU", position="QB")
+
+    #def test_listing(self):
+
+    #def test_search_name(self):
+
+    #def test_filter_position(self):
+
+    #TODO dont think we need testing for initial POC
+
+    
--- draftvision_backend/api/views.py
+++ draftvision_backend/api/views.py
@@ -1,28 +1,61 @@
 from rest_framework import viewsets, filters
+from rest_framework import status
 from .models import Player
 from .serializers import PlayerSerializer
 from rest_framework.decorators import action
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 class PlayerViewSet(viewsets.ReadOnlyModelViewSet):
+    #Getting set of all players
     queryset = Player.objects.all().select_related('details')
     serializer_class = PlayerSerializer
     
     def get_queryset(self):
         queryset = Player.objects.all()
-        # Filters matching your UI
-        year = self.request.query_params.get('year', None)
-        position = self.request.query_params.get('position', None)
-        conference = self.request.query_params.get('conference', None)
-        team = self.request.query_params.get('team', None)
+        # Filters, need to see with UI how many and what filters we want
+        filter_year = self.request.query_params.get('year', None)
+        filter_position = self.request.query_params.get('position', None)
+        filter_conference = self.request.query_params.get('conference', None)
+        filter_team = self.request.query_params.get('team', None)
         
-        if year:
-            queryset = queryset.filter(year=year)
-        if position:
-            queryset = queryset.filter(position=position)
-        if conference:
-            queryset = queryset.filter(college__conference=conference)
-        if team:
-            queryset = queryset.filter(college=team)
+        #Filtering queryset based on what is inputted on site
+        if filter_year:
+            queryset = queryset.filter(year=filter_year)
+        if filter_position:
+            queryset = queryset.filter(position=filter_position)
+        if filter_conference:
+            queryset = queryset.filter(college__conference=filter_conference)
+        if filter_team:
+            queryset = queryset.filter(college=filter_team)
             
         return queryset.order_by('-player_rating')
+    
+    #Get player by their id
+    def get_player(request, id):
+        try:
+            #searching the model based on id
+            player = Player.objects.get(id=id)
+            serializer = PlayerSerializer(player)
+            return Response(serializer.data)
+        #Exceptions for if id does not exist
+        except Player.DoesNotExist:
+            return Response({"error": "Player not found"}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    #Search for a player based on name, will be important later for mock draft
+    def get_player_named(request):
+        try:
+            #Trying to get set of players based on if string is contained in the name
+            name = request.GET.get('name', '').strip()
+            players = Player.objects.filter(name=name) #TODO dont know if this is case insensitive or not
+            if players.exists():
+                serializer = PlayerSerializer(players, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "No players found"}, status=status.HTTP404_NOT_FOUND)
+            #Exceptions
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
