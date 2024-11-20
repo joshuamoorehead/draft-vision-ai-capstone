8c8cf65127e9872d42b3808a71222d4b9f09af4c
zakelguindi <zakelguindi@gmail.com>
2024-11-19 23:27:11 -0500

    adding csv's to my branch

--- testingdatasets.py
+++ testingdatasets.py
@@ -12,16 +12,23 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 
 # base_url = f"https://api.collegefootballdata.com/bearer={api_key}records?year=2022&conference=SEC"
-# config = cfbd.Configuration()
-# config.api_key['Authorization'] = api_key
-# config.api_key_prefix['Authorization'] = 'Bearer'
+config = cfbd.Configuration()
+config.api_key['Authorization'] = api_key
+config.api_key_prefix['Authorization'] = 'Bearer'
 
-# api_instance = cfbd.RatingsApi(cfbd.ApiClient(config))
-# conf = api_instance.get_conference_sp_ratings(year=2022, conference='SEC')
+api_instance = cfbd.DraftApi(cfbd.ApiClient(config))
+conf = api_instance.get_draft_picks(year=2022, conference='SEC', position='Defensive End')
 
 # for d in conf:
-#     print(d)
-#     print('-----------')
+#     temp = d.__dict__
+#     if temp['_player'] == 'Jalen Carter':
+#         print(f'{temp['_stat_type']}:\t{temp['_stat']}')
+#         print('-----------')
+
+for d in conf:
+    temp = d.__dict__
+    print(f'{temp['_overall']}\n{temp['_pre_draft_ranking']}\n{temp['_pre_draft_position_ranking']}\n{temp['_pre_draft_grade']}')
+    print('-------------')
 
 # team = api_instance.get_sp_ratings(year=2023, team='Georgia')
 # for t in team:
