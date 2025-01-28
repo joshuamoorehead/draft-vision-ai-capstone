79c9021698e407c850364860ffa14c47fa665351
Tyler Super <tsuper@email.sc.edu>
2025-01-28 16:32:43 -0500

    Changes to unit testing #51

--- dv/db/tests.py
+++ dv/db/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
-from models import Team,TeamYear,DraftInfo, Player, PassingStats, RushingStats, ReceivingStats
-from serializers import TeamSerializer, TeamYearSerializer, DraftInfoSerializer, PlayerSerializer, PassingStatsSerializer, RushingStatsSerializer, ReceivingStatsSerializer
+from .models import PlayerProfile, NCAATeams
+from .serializers import PlayerProfileSerializer, NCAATeamsSerializer
 
 
 # Create your tests here.
@@ -10,22 +10,22 @@ from serializers import TeamSerializer, TeamYearSerializer, DraftInfoSerializer,
 class PlayerAPITest(APITestCase):
     def setUp(self):
         #Create test player with name position and college filled out, all other parameters default
-        self.player = Player.objects.create(
+        self.player = PlayerProfile.objects.create(
             name="Joe Football", 
-            college="LSU", 
+            school="LSU", 
             position="QB",)
 
     def test_player_creation(self):
-        player = Player.objects.get(name="Joe Football")
-        self.assertEqual(player.team, "LSU")
+        player = PlayerProfile.objects.get(name="Joe Football")
+        self.assertEqual(player.school, "LSU")
         self.assertEqual(player.position, "QB")
 
     def test_filter_position(self):
-        quarterbacks = Player.objects.filter(position="QB")
+        quarterbacks = PlayerProfile.objects.filter(position="QB")
         self.assertEqual(quarterbacks.count(),1)
         self.assertEqual(quarterbacks.first().name, "Joe Football")
 
     def test_filter_team(self):
-        LSU_team = Player.objects.filter(team="LSU")
+        LSU_team = PlayerProfile.objects.filter(team="LSU")
         self.assertEqual(LSU_team.count(), 1)
         self.assertEqual(LSU_team.first().name, "Joe Football")
